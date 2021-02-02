d2d6277affac0132b7d8778fc7d2e7940914d788
Tim Sullivan <timsullivan97@gmail.com>
2021-02-01 16:47:55 -0800

    tictactoe.js: loop tic tac toe game

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -9,6 +9,8 @@ function prompt(msg) {
 }
 
 function displayBoard(board) {
+  console.clear();
+
   console.log("");
   console.log("     |     |");
   console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]} `);
@@ -59,10 +61,21 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) {
+  return false;
+}
+
 let board = initializeBoard();
 displayBoard(board);
 
-playerChoosesSquare(board);
-computerChoosesSquare(board);
+while (true) {
+  playerChoosesSquare(board);
+  computerChoosesSquare(board);
+  displayBoard(board);
 
-displayBoard(board);
+  if (someoneWon(board) || boardFull(board)) break;
+}
