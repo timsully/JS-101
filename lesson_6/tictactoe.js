cbfc3677a54dd689f3e007b41567345652803bde
Tim Sullivan <timsullivan97@gmail.com>
2021-02-01 16:36:02 -0800

    tictactoe.js: add computer and player choices

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,27 +1,25 @@
-// let board = {
-//   1: "X", // top left
-//   2: " ", // top center
-//   3: " ", // top right
-//   4: " ", // middle left
-//   5: "O", // center
-//   6: " ", // middle right
-//   7: " ", // bottom left
-//   8: " ", // bottom center
-//   9: "X", // bottom right
-// };
+let readline = require("readline-sync");
+
+const INITIAL_MARKER = " ";
+const HUMAN_MARKER = "X";
+const COMPUTER_MARKER = "O";
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
 
 function displayBoard(board) {
   console.log("");
   console.log("     |     |");
-  console.log(`  ${board["1"]}  |   ${board["2"]} |   ${board["3"]}`);
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]} `);
   console.log("     |     |");
   console.log("-----+-----+-----");
   console.log("     |     |");
-  console.log(` ${board['4']}   |  ${board['5']}  |   ${board['6']}`);
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]} `);
   console.log("     |     |");
   console.log("-----+-----+-----");
   console.log("     |     |");
-  console.log(` ${board['7']}   |   ${board['8']} |  ${board['9']}`);
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
   console.log("     |     |");
   console.log("");  
 }
@@ -30,11 +28,41 @@ function initializeBoard() {
   let board = {};
 
   for (let square = 1; square <= 9; square++) {
-    board[String(square)] = ' ';
+    board[String(square)] = INITIAL_MARKER;
   }
 
   return board;
 }
 
+function emptySquares(board) {
+  return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
+}
+
+function playerChoosesSquare(board) {
+  let square;
+
+  while (true) {
+    prompt(`Choose a square (${emptySquares(board).join(', ')}):`);
+    square = readline.question().trim();
+    if (emptySquares(board).includes(square)) break;
+
+    prompt("Sorry, that's not a valid choice.");
+  }
+
+  board[square] = HUMAN_MARKER;
+}
+
+function computerChoosesSquare(board) {
+  let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+
+  let square = emptySquares(board)[randomIndex];
+  board[square] = COMPUTER_MARKER;
+}
+
 let board = initializeBoard();
+displayBoard(board);
+
+playerChoosesSquare(board);
+computerChoosesSquare(board);
+
 displayBoard(board);
