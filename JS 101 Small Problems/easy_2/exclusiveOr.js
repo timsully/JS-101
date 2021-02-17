fa5d793945bc0f0d1f3e57d5b07ff0168c4d9f17
Tim Sullivan <timsullivan97@gmail.com>
2021-02-17 10:46:55 -0800

    update

--- JS 101 Small Problems/easy_2/exclusiveOr.js
+++ JS 101 Small Problems/easy_2/exclusiveOr.js
@@ -12,6 +12,7 @@ function xor(value1, value2) {
 }
 
 console.log(xor(5, 0) === true);
+console.log(xor(0, 5) === true);
 console.log(xor(false, true) === true);
 console.log(xor(1, 1) === false);
 console.log(xor(true, true) === false);
