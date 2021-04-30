6ce26f058208cb09f3579741b15c9562355cedcc
Tim Sullivan <timsullivan97@gmail.com>
2021-04-29 22:56:34 -0700

    add exercise

---
+++ JS 101 Small Problems/easy_5/halvsies.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes an array as an argument and returns an array that contains two elements, each of which is an array. Put the first half of the original array elements in the first element of the return value, and put the second half in the second element. If the original array contains an odd number of elements, place the middle element in the first half array.
+*/
+
+
+function halvsies(array) {
+  let half = Math.ceil(array.length / 2);
+  let firstHalf = array.slice(0, half);
+  let secondHalf = array.slice(half);
+  return [firstHalf, secondHalf];
+}
+
+
+halvsies([1, 2, 3, 4]);       // [[1, 2], [3, 4]]
+halvsies([1, 5, 2, 4, 3]);    // [[1, 5, 2], [4, 3]]
+halvsies([5]);                // [[5], []]
+halvsies([]);                 // [[], []]
